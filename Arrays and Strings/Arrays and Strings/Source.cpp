c392956f0867fe2dfd350bab2b58d395a4841b8e
Tony Nguyen <tbnktd@umsystem.edu>
2023-03-26 10:35:52 -0500

    .

--- Arrays and Strings/Arrays and Strings/Source.cpp
+++ Arrays and Strings/Arrays and Strings/Source.cpp
@@ -7,6 +7,14 @@ int main() {
 
 	int arr[5] = { 5, 8, 3, 2, 10 };
 
+	for (int i = 0; i < 5; i++) {
+		i++
+	}
+	cout << i;
 
 
+
+	//cout << "Enter the size of the array: "
+	//cout << "Enter the elements of the array: "
+	//cout << "The maximum is: " 
 }
